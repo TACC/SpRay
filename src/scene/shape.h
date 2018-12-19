6931d20b006f2fa6761bc00436211b021aa2bc22
Hyungman Park <hyungman@utexas.edu>
2018-12-19 12:04:17 -0600

    added Shape class

--- src/scene/shape.h
+++ src/scene/shape.h
@@ -20,41 +20,16 @@
 
 #pragma once
 
-#include <string>
-#include <vector>
-
-#include "glm/glm.hpp"
-
-#include "partition/aabb.h"
-#include "scene/shape.h"
-
 namespace spray {
 
-class Bsdf;
-class Light;
-
-class Shape;
-
-class Domain {
+class Shape {
  public:
-  Domain() : num_vertices(0), num_faces(0), bsdf(nullptr) {}
-  ~Domain() {
-    for (std::size_t i = 0; i < shapes.size(); ++i) delete shapes[i];
-  }
-  unsigned id;  // single domain id
-  std::size_t num_vertices;
-  std::size_t num_faces;
-  std::string filename;
-  Aabb object_aabb;
-  Aabb world_aabb;
-  glm::mat4 transform;
-  Bsdf* bsdf;
-  std::vector<Shape*> shapes;
+  Shape() {}
 };
 
-void loadDescriptor(const std::string& filename, const std::string& ply_path,
-                    std::vector<Domain>* domains_out,
-                    std::vector<Light*>* lights_out);
+class Sphere : public Shape {
+ public:
+  Sphere() : Shape() {}
+};
 
 }  // namespace spray
-
