d7ed4a3eb9763198d63597b983509d67d9550d7d
Hyungman <hyungman@utexas.edu>
2018-12-31 21:29:18 -0600

    updated spheres.sh so user can choose a light source (diffuse or point light source)

--- examples/spheres/spheres.sh
+++ examples/spheres/spheres.sh
@@ -10,7 +10,8 @@ SPRAY_BIN_PATH=$SPRAY_HOME_PATH/build
 SHAPES_APP=$SPRAY_BIN_PATH/spray_insitu_shapes
 EXAMPLE_PATH=$SPRAY_HOME_PATH/examples
 SPHERES_PATH=$SPRAY_HOME_PATH/examples/spheres
-SCENE=$SPHERES_PATH/spheres.spray
+SCENE_DIFFUSE_LIGHT=$SPHERES_PATH/spheres-diffuse-light.spray
+SCENE_POINT_LIGHT=$SPHERES_PATH/spheres-point-light.spray
 MPI_BIN="mpirun -n"
 
 echo "Choose a setup (1-2):"
@@ -19,6 +20,25 @@ echo "2. simple diffuse, antialiasing, glfw"
 echo "3. simple diffuse, antialiasing, film"
 
 read APP
+
+echo "Choose a light (1-2):"
+echo "1. point light" 
+echo "2. diffuse light"
+
+read LIGHT
+
+if [ $LIGHT == "1" ] 
+then
+  SCENE=$SCENE_POINT_LIGHT
+
+elif [ $LIGHT == "2" ]
+then
+  SCENE=$SCENE_DIFFUSE_LIGHT
+
+else
+  echo "[error] invalid input"
+  return
+fi
 
 # cmd="$SPRAY_BIN_PATH/spray_insitu_shapes --nthreads 1 -w 512 -h 512 --frames -1 --mode glfw --cache-size -1 --partition insitu --camera 0 0 5 0 0 0 --pixel-samples 1 --ao-samples 1 --ao-mode --bounces 1 --blinn 0.4 0.4 0.4 10 --num-partitions 1 $SPRAY_HOME_PATH/examples/spheres/spheres.spray"
 
