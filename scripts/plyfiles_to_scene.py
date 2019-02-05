004b94ef61fd681f426a4fa207361ee8234a412b
Hyungman <hyungman@utexas.edu>
2019-02-05 15:58:47 -0600

    updated scene file generator

--- scripts/plyfiles_to_scene.py
+++ scripts/plyfiles_to_scene.py
@@ -24,90 +24,176 @@ import os
 import sys
 import argparse
 # import subprocess
+import re
 
-parser = argparse.ArgumentParser(description='Domains maker')
-parser.add_argument('--indir', nargs='+', required=True, help='a list of directories containing ply files')
-parser.add_argument('--loader', nargs=1, required=True, help='executable for ply loader')
-parser.add_argument('--out', nargs=1, default=['scene.domain'], help='output file')
-parser.add_argument('--abspath', action='store_true', help='use absoulte path for ply files')
+def addLights(outfile):
+  with open(outfile, "w") as fout:
+    fout.write("light diffuse-sphere 1 1 1\n")
 
+def getDomainId(filename):
+  # dns_r0180_c000_d0002_iso0.8.ply
+  found = re.search('_d([0-9]+)_', filename)
+  return int(found.group(1))
 
-# parser.add_argument('--scale', nargs=3, type=float, help='scaling factor (x,y,z)')
-# parser.add_argument('--gap', nargs=1, type=float, default=[1.0], help='gap factor (between domains)')
-args = parser.parse_args()
+def getIsoValue(filename):
+  # dns_r0180_c000_d0002_iso0.8.ply
+  found = re.search('_iso([0-9.]+).ply', filename)
+  return float(found.group(1))
 
-# print("program dump_ply_info indir [outfile]")
+def loadPlyFilesAndGenerateScene(args, plyfiles, loader_executable, outfile):
+  total_num_faces = 0 
+  total_num_vertices = 0 
 
-# if len(sys.argv) < 3:
-#   print("[error] no input directory found")
-#   sys.exit(1)
+  for domain_id in sorted(plyfiles):
 
-# program to dump info
-exe_name = args.loader[0]
+    with open(outfile, "a") as fout:
+      fout.write("######################\n")
+      fout.write("# " + str(domain_id) + "\n")
+      fout.write("DomainBegin\n")
 
+    domain_num_faces = 0 
+    domain_num_vertices = 0
 
-print("[python] listing ply files")
+    for ply, iso in plyfiles[domain_id]:
+      cmd = loader_executable + " " + ply + " out.tmp"
+      print("[python] " + cmd)
+      os.system(cmd)
+      
+      lines=[] 
+      with open("./out.tmp", "r") as ftemp:
+        for line in ftemp:
+          lines.append(line)
+      
+      os.system("rm -f ./out.tmp")
 
-ply_dirs = args.indir
-plyfiles=[]
-plyfiles_abspath=[]
+      num_faces = 0 
+      num_vertices = 0
 
-for ply_dir in ply_dirs:
-  filenames = os.listdir(ply_dir)
-  dir_name = os.path.abspath(ply_dir)
+      for line in lines:
+        if "face" in line:
+          num_faces = int(line.split()[1])
+          total_num_faces += num_faces
+          domain_num_faces += num_faces
+        if "vertex" in line:
+          num_vertices = int(line.split()[1])
+          total_num_vertices += num_vertices
+          domain_num_vertices += num_vertices
+        # if (not args.abspath) and ("file" in line):
+        #   filename_only = os.path.basename(line.split()[1])
+        #   ftemp.write("file " + filename_only + "\n")
+        # else:
+        #   ftemp.write(line)
 
-  c=0  
-  for filename in filenames:
-    if filename.endswith(".ply"):
-      plyfile_name = os.path.join(dir_name, filename)
-      plyfiles.append(plyfile_name)
-      print("[python] " + plyfile_name)
-      c += 1
+      with open(outfile, "a") as fout:
+        fout.write("ModelBegin\n")
+      
+        if args.abspath:
+          fout.write("file " + ply + "\n")
+        else:
+          fout.write("file " + os.path.basename(ply) + "\n")
+      
+        fout.write("material matte\n")
+        fout.write("# number of vertices: " + str(num_vertices)  + "\n")
+        fout.write("# number of faces: " + str(num_faces)  + "\n")
+        fout.write("ModelEnd\n")
+
+    with open(outfile, "a") as fout:
+      fout.write("# [domain " + str(domain_id) + "] number of vertices: " + str(domain_num_vertices)  + "\n")
+      fout.write("# [domain " + str(domain_id) + "] number of faces: " + str(domain_num_faces)  + "\n")
+      fout.write("DomainEnd\n")
+
+  with open(outfile, "a") as fout:
+    fout.write("######################\n")
+    fout.write("# total vertices " + str(total_num_vertices) + "\n")
+    fout.write("# total faces " + str(total_num_faces) + "\n")
+
+def generateScene(args, plyfiles, outfile):
+  for domain_id in sorted(plyfiles):
+    with open(outfile, "a") as f:
+      f.write("######################\n")
+      f.write("# " + str(domain_id) + "\n")
+      f.write("DomainBegin\n")
+    for ply, iso in plyfiles[domain_id]:
+      with open(outfile, "a") as f:
+        f.write("ModelBegin\n")
+      
+        if args.abspath:
+          f.write("file " + ply + "\n")
+        else:
+          f.write("file " + os.path.basename(ply) + "\n")
+      
+        f.write("material matte\n")
+        f.write("ModelEnd\n")
+    with open(outfile, "a") as f:
+      f.write("DomainEnd\n")
   
-  if c == 0: 
-    print("empty directory " + ply_dir)
+def main():
+  parser = argparse.ArgumentParser(description='Domains maker')
+  parser.add_argument('--indir', nargs='+', required=True, help='a list of directories containing ply files')
+  parser.add_argument('--loader', nargs=1, required=False, help='executable for ply loader')
+  parser.add_argument('--out', nargs=1, default=['scene.domain'], help='output file')
+  parser.add_argument('--abspath', action='store_true', help='use absoulte path for ply files')
+  
+  
+  # parser.add_argument('--scale', nargs=3, type=float, help='scaling factor (x,y,z)')
+  # parser.add_argument('--gap', nargs=1, type=float, default=[1.0], help='gap factor (between domains)')
+  args = parser.parse_args()
+  
+  # print("program dump_ply_info indir [outfile]")
+  
+  # if len(sys.argv) < 3:
+  #   print("[error] no input directory found")
+  #   sys.exit(1)
+  
+  # program to dump info
+  if args.loader:
+    loader_executable = args.loader[0]
+  
+  outfile = args.out[0]
+  if os.path.isfile(outfile):
+    print("[error] output file already exists: " + outfile)
     sys.exit(1)
+  # os.system("rm -f " + outfile)
 
-
-outfile = args.out[0]
-os.system("rm -f " + outfile)
-
-n = 0 
-num_faces = 0 
-num_vertices = 0 
-for ply in plyfiles:
-  cmd = exe_name + " " + ply + " out.tmp"
-  print("[python] " + cmd)
-  os.system(cmd)
-
-  lines=[] 
-  with open("./out.tmp", "r") as f:
-    for line in f:
-      lines.append(line)
-
-  os.system("rm -f ./out.tmp")
-
-  with open(outfile, "a") as f:
-    f.write("######################\n")
-    f.write("# " + str(n) + "\n")
-    f.write("domain\n")
-    for line in lines:
-      if (not args.abspath) and ("file" in line):
-        filename_only = os.path.basename(line.split()[1])
-        f.write("file " + filename_only + "\n")
-      else:
-        f.write(line)
-      if "face" in line:
-        num_faces += int(line.split()[1])
-      if "vertex" in line:
-        num_vertices += int(line.split()[1])
-    f.write("mtl diffuse 1 1 1\n")
+  print("[python] listing ply files")
   
-  n += 1
+  ply_dirs = args.indir
+  plyfiles={}
+  plyfiles_abspath=[]
+  
+  for ply_dir in ply_dirs:
+    filenames = os.listdir(ply_dir)
+    dir_name = os.path.abspath(ply_dir)
+  
+    c=0  
+    for filename in filenames:
+      if filename.endswith(".ply"):
+        plyfile_name = os.path.join(dir_name, filename)
+        domain_id = getDomainId(filename)
+        iso_value = getIsoValue(filename)
 
-with open(outfile, "a") as f:
-  f.write("######################\n")
-  f.write("# total vertices " + str(num_vertices) + "\n")
-  f.write("# total faces " + str(num_faces) + "\n")
+        if domain_id in plyfiles:
+          plyfiles[domain_id].append((plyfile_name, iso_value))
+        else:
+          plyfiles[domain_id] = [(plyfile_name, iso_value)]
 
-print("[info] generated a scene file in " + outfile)
+        print("[python] " + plyfile_name)
+        c += 1
+    
+    if c == 0: 
+      print("[warning] empty directory " + ply_dir)
+      # sys.exit(1)
+
+  # add lights
+  addLights(outfile)
+  
+  if args.loader:
+    loadPlyFilesAndGenerateScene(args, plyfiles, loader_executable, outfile)
+  else:
+    generateScene(args, plyfiles, outfile)
+      
+  print("[info] generated a scene file in " + outfile)
+
+if __name__ == '__main__':
+  main()
+
